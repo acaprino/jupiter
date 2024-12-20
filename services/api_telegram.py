84e3a8b994d9c4890238d772ade917d427bed352
Alfio Caprino <alfio.caprino@gmail.com>
2024-12-20 20:01:37 +0100

    Code update

--- services/api_telegram.py
+++ services/api_telegram.py
@@ -1,4 +1,5 @@
 import asyncio
+import threading
 
 from aiogram.exceptions import TelegramRetryAfter, TelegramServerError
 from aiohttp import ClientConnectionError
@@ -9,38 +10,54 @@ from misc_utils.error_handler import exception_handler
 
 class TelegramAPIManager:
     _instance = None
-    _lock = asyncio.Lock()
+    _lock = threading.Lock()
+    _initialized = False
 
     def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(TelegramAPIManager, cls).__new__(cls)
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super(TelegramAPIManager, cls).__new__(cls)
         return cls._instance
 
     def __init__(self):
+        if TelegramAPIManager._initialized:
+            return
         self.queue = None
         self.worker_task = None
-        self._initialized = False
+        self.logger = BotLogger(name="TelegramAPIManager")
+        TelegramAPIManager._initialized = True
+        self.logger.info("TelegramAPIManager instance created.")
 
     @exception_handler
     async def initialize(self):
-        if self._initialized:
+        self.logger.info("Called initialize function")
+        if hasattr(self, '_initialized') and self.queue is not None:
+            self.logger.info("Already initialized. Skipping initialization.")
             return
+
+        self.logger.info("Initializing...")
         self.queue = asyncio.Queue()
         self.worker_task = asyncio.create_task(self._process_queue())
-        self._initialized = True
+        self.logger.info("Initialization complete.")
 
     @exception_handler
     async def enqueue(self, method, agent, *args, **kwargs):
         await self.queue.put((method, agent, args, kwargs))
 
     async def _process_queue(self):
+        self.logger.info("_process_queue started")
         while True:
             method, agent, args, kwargs = await self.queue.get()
             try:
+                self.logger.info(f"Processing {method}, {agent}, {args}, {kwargs}")
                 await self._execute_api_call(method, agent, *args, **kwargs)
+            except asyncio.CancelledError:
+                self.logger.debug("_process_queue cancelled")
+                raise
             except Exception as e:
                 BotLogger.get_logger(agent).critical(f"Error processing API call in _process_queue: {e}")
             finally:
+                self.logger.info(f"Task processed {method}, {agent}, {args}, {kwargs}")
                 self.queue.task_done()
 
     @exception_handler
@@ -54,24 +71,28 @@ class TelegramAPIManager:
                 return
             except TelegramRetryAfter as e:
                 wait_time = e.retry_after
-                logger.warning(f"Rate limit exceeded. Retrying after {wait_time} seconds...")
+                self.logger.warning(f"Rate limit exceeded. Retrying after {wait_time} seconds...")
                 await asyncio.sleep(wait_time)
             except (TelegramServerError, ClientConnectionError) as e:
-                logger.error(f"Temporary error: {e}. Retrying in 5 seconds...")
+                self.logger.error(f"Temporary error: {e}. Retrying in 5 seconds...")
                 await asyncio.sleep(5)
             except Exception as e:
-                logger.critical("Unexpected error during API call:")
+                self.logger.critical("Unexpected error during API call:")
                 raise  # Re-raise the exception to be caught in _process_queue
             retries += 1
-        logger.error("Exceeded maximum retries for API call.")
+        self.logger.error("Exceeded maximum retries for API call.")
 
     @exception_handler
     async def shutdown(self):
+        self.logger.info("Shutting down TelegramAPIManager.")
         if self.worker_task:
             self.worker_task.cancel()
             try:
                 await self.worker_task
+                self.logger.debug("Worker task successfully cancelled.")
             except asyncio.CancelledError:
-                pass
-            self._initialized = False
-            self.queue = None
+                self.logger.debug("Worker task cancellation confirmed.")
+            except Exception as e:
+                self.logger.critical(f"Error during worker task cancellation: {e}")
+        else:
+            self.logger.debug("No worker task to cancel.")
