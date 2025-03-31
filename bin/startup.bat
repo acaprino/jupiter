5f2238766cbc6a3c721ab96ea1fd499c04c6dd86
acaprino <94200626+acaprino@users.noreply.github.com>
2025-03-31 14:47:15 +0200

    Code update

--- bin/startup.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-start "Bot Service" python startup.py
--- git_ingest.py
+++
@@ -1,4 +0,0 @@
-# Synchronous usage
-from gitingest import ingest
-
-summary, tree, content = ingest(source=".", output="out.txt")
