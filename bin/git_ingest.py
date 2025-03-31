0ed3ee6c3a71a8cd259acb1000e5073e04758c2d
acaprino <94200626+acaprino@users.noreply.github.com>
2025-03-31 14:54:41 +0200

    Code update

--- bin/git_ingest.py
+++ bin/git_ingest.py
@@ -1,4 +1,4 @@
 # Synchronous usage
 from gitingest import ingest
 
-summary, tree, content = ingest(source="..", output="out.txt", exclude_patterns={"*.csv", "*.log"})
+summary, tree, content = ingest(source="..", output="out.txt", exclude_patterns={"*.csv", "*.log", "*.log.*"})
