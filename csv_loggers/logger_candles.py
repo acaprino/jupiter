1316f1381e9d7070484ee6ee77ac71b52adfcf65
acaprino <94200626+acaprino@users.noreply.github.com>
2025-04-17 11:20:02 +0200

    Code update

--- csv_loggers/logger_candles.py
+++ csv_loggers/logger_candles.py
@@ -6,11 +6,14 @@ from misc_utils.config import ConfigReader
 
 class CandlesLogger(CSVLogger):
 
-    def __init__(self, config: ConfigReader, symbol, timeframe, trading_direction, custom_name=''):
+    def __init__(self, config: ConfigReader, symbol, timeframe, trading_direction, custom_name=None):
         timeframe = timeframe.name
         trading_direction = trading_direction.name
+        bot_name = config.get_bot_name()
+        instance_name = config.get_instance_name()
+        custom_name = f"_{custom_name}" if custom_name is not None else ''
         output_path = None
-        logger_name = f'candles_{symbol}_{timeframe}_{trading_direction}_{custom_name}'
+        logger_name = f'candles{custom_name}_{bot_name}_{instance_name}_{symbol}_{timeframe}_{trading_direction}'
         super().__init__(config, logger_name, output_path, real_time_logging=True, max_bytes=10 ** 6, backup_count=10, memory_buffer_size=0)
 
     def add_candle(self, candle: Series):
--- csv_loggers/logger_rabbit_messages.py
+++ csv_loggers/logger_rabbit_messages.py
@@ -4,10 +4,12 @@ from misc_utils.config import ConfigReader
 
 class RabbitMessages(CSVLogger):
 
-    def __init__(self, config: ConfigReader, custom_name=''):
+    def __init__(self, config: ConfigReader, custom_name=None):
         bot_name = config.get_bot_name()
+        instance_name = config.get_instance_name()
+        custom_name = f"_{custom_name}" if custom_name is not None else ''
         output_path = None
-        logger_name = f'messages_{bot_name}_{custom_name}'
+        logger_name = f'messages{custom_name}_{bot_name}_{instance_name}'
         super().__init__(config, logger_name, output_path, real_time_logging=True, max_bytes=10 ** 6, backup_count=10, memory_buffer_size=0)
 
     def add_message(self, exchange: str,
